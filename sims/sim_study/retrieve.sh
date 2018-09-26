68c13afec751f72afc20b082fffc22f4721e732b
Arthur Lui <luiarthur@gmail.com>
2018-09-26 12:23:36 -0700

    put the buckets in last line

--- sims/sim_study/retrieve.sh
+++ sims/sim_study/retrieve.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+AWS_BUCKET=$1
+aws s3 sync $AWS_BUCKET result/
+
 # For example,
 # AWS_BUCKET="s3://cytof-results/Wed-Sep-19-20:46:41-PDT-2018/"
-AWS_BUCKET=$1
-
-aws s3 sync $AWS_BUCKET result/
+# AWS_BUCKET="s3://cytof-results/sim/complexZ_thin10y/"
--- sims/sim_study/sendToAWS.sh
+++ sims/sim_study/sendToAWS.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-#DEST="s3://cytof-results/sim/complexZ_thin10y/"
 DEST=$1
 aws s3 sync result $DEST
+
+#DEST="s3://cytof-results/sim/complexZ_thin10y/"
