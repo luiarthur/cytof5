103e7774effa574acb712e5c6ef1070b60cac310
Arthur Lui <luiarthur@gmail.com>
2018-09-27 14:48:38 -0700

    edits to retrieve script

--- sims/sim_study/retrieve.sh
+++ sims/sim_study/retrieve.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-AWS_BUCKET=$1
-aws s3 sync $AWS_BUCKET result/
+if [ "$#" -ne 1 ]; then
+  echo "usage: . retrieve.sh <bucket/path>"
+  echo "Here are the buckets available in s3://cytof-results/"
+  aws s3 ls s3://cytof-results/ --recursive
+else
+  AWS_BUCKET=$1
+  aws s3 sync $AWS_BUCKET result/
+fi
 
-# For example,
-# AWS_BUCKET="s3://cytof-results/Wed-Sep-19-20:46:41-PDT-2018/"
+# All buckets:
 # AWS_BUCKET="s3://cytof-results/sim/complexZ_thin10y/"
--- sims/sim_study/sendToAWS.sh
+++ sims/sim_study/sendToAWS.sh
@@ -3,4 +3,5 @@
 DEST=$1
 aws s3 sync result $DEST
 
+# All buckets:
 #DEST="s3://cytof-results/sim/complexZ_thin10y/"
