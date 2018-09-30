0e60c65aec74c8148eb5a98e9cc1a9382c5abcc1
luiarthur <luiarthur@gmail.com>
2018-09-29 20:25:45 -0700

    made scripts to run cb

--- sims/cb/cb.jl
+++ sims/cb/cb.jl
@@ -3,24 +3,92 @@ Pkg.activate("../../")
 
 using Cytof5, Random, RCall
 using JLD2, FileIO
+using ArgParse
 
-Random.seed!(10)
-printDebug = false
-
-# Read CB Data
-cbDataPath = "/soe/alui2/repo/ucsc_litreview/cytof/src/model3/sims/data/cytof_cb.rds";
-R"cbData_R = readRDS(file=$cbDataPath)";
-markers = R"colnames(cbData_R[[1]])";
-cbData = [Matrix{Union{Float64, Missing}}(reshape(yi .+ 0, size(yi))) for yi in R"cbData_R"];
-R"rm(cbData_R)"
-
-# Get Dimensions
-I = length(cbData)
-J = size(cbData[1], 2)
-N = [size(y, 1) for y in cbData]
-
-for i in 1:I
-    idx_missing = isnan.(cbData[i])
-    cbData[i][idx_missing] .= missing
+function logger(x; newline=true)
+  if newline
+    println(x)
+  else
+    print(x)
+  end
+  flush(stdout)
 end
 
+
+# ARG PARSING
+function parse_cmd()
+  s = ArgParseSettings()
+
+  @add_arg_table s begin
+    "--K_MCMC"
+      arg_type = Int
+      required = true
+    "--L_MCMC"
+      arg_type = Int
+      required = true
+    "--b0PriorSd"
+      arg_type = Float64
+      default = 1.0
+    "--b1PriorScale"
+      arg_type = Float64
+      default = 1.0
+    "--SEED"
+      arg_type = Int
+      default = 0
+
+    "--RESULTS_DIR"
+      arg_type = String
+      required = true
+    "--EXP_NAME"
+      arg_type = String
+      required = true
+  end
+
+  return parse_args(s)
+end
+
+PARSED_ARGS = parse_cmd()
+for (k,v) in PARSED_ARGS
+  logger("$k => $v")
+end
+
+K_MCMC = PARSED_ARGS["K_MCMC"]
+L_MCMC = PARSED_ARGS["L_MCMC"]
+b0PriorSd = PARSED_ARGS["b0PriorSd"]
+b1PriorScale = PARSED_ARGS["b1PriorScale"]
+SEED = PARSED_ARGS["SEED"]
+RESULTS_DIR = PARSED_ARGS["RESULTS_DIR"]
+EXP_NAME = PARSED_ARGS["EXP_NAME"]
+
+Random.seed!(SEED);
+# End of ArgParse
+
+# Read CB Data
+cbDataPath = "data/cytof_cb.jld2"
+@load cbDataPath y_cb
+dat = Cytof5.Model.Data(y_cb)
+
+# MAIN
+logger("Generating priors ...");
+@time c = Cytof5.Model.defaultConstants(dat, K_MCMC, L_MCMC, b0PriorSd=b0PriorSd, b1PriorScale=b1PriorScale)
+
+logger("Generating initial state ...");
+@time init = Cytof5.Model.genInitialState(c, dat)
+
+logger("Fitting Model ...");
+@time out, lastState, ll = Cytof5.Model.cytof5_fit(init, c, dat,
+                                                   monitors=[[:Z, :lam, :W,
+                                                              :b0, :b1, :v,
+                                                              :sig2, :mus,
+                                                              :alpha, :v,
+                                                              :eta],
+                                                             [:y_imputed]],
+                                                   thins=[1, 100],
+                                                   nmcmc=1000, nburn=15000,
+                                                   numPrints=1000,
+                                                   flushOutput=true)
+
+logger("Saving Data ...");
+@save "$(OUTDIR)/output.jld2" out ll lastState c
+
+logger("MCMC Completed.");
