8c3db74402f65ec0f0a0ba2fe0e0391c3ab8e25b
Arthur Lui <luiarthur@gmail.com>
2018-10-15 22:37:23 -0700

    made progress with pyplot.jl

--- sandbox/pyplot/plots.jl
+++ sandbox/pyplot/plots.jl
@@ -5,35 +5,58 @@ printKeys(x) = [print("$k  ") for k in keys(x)]
 plt.ioff() # turn off interactive graphics. turn on with plt.ion()
 =#
 
+function pyRange(n::Int)
+  return collect(0:(n-1))
+end
+
+
+function plotZ(Z::Matrix{T}) where {T <: Number}
+  J, K = size(Z)
+  cm = plt.cm_get_cmap(:Greys)
+  img = plt.imshow(Z, aspect="auto", vmin=0, vmax=1, cmap=cm);
+  ax = plt.gca()
+  [ plt.axhline(y=i+.5, color="grey", linewidth=.5) for i in pyRange(J)];
+  [ plt.axvline(x=i+.5, color="grey", linewidth=.5) for i in pyRange(K)];
+  plt.yticks(pyRange(J), 1:J);
+  plt.xticks(pyRange(K), 1:K, rotation=:vertical);
+  return img
+end
+
+function plotY(Y::Matrix{T}, clim=(-3,3)) where {T  <: Number}
+  cm = plt.cm_get_cmap(:bwr)
+  cm[:set_under](color=:blue)
+  cm[:set_over](color=:red)
+  cm[:set_bad](color=:black)
+  img = plt.imshow(Y, vmin=clim[1], vmax=clim[2], aspect="auto", cmap=cm)
+  plt.plt[:colorbar]();
+  return img
+end
+
 ### IBP ###
 J = 32
 K = 8
 Z = Int.(rand(J, K) .> .6);
 
-plt.imshow(Z, aspect="auto", vmin=.01, vmax=.99);
-ax = plt.gca()
-cm = plt.cm_get_cmap()
-cm[:set_under](color="white")
-cm[:set_over](color="black")
-[ plt.axhline(y=i+.5, color="grey", linewidth=.5) for i in 0:(J-1)];
-[ plt.axvline(x=i+.5, color="grey", linewidth=.5) for i in 0:(K-1)];
-plt.xticks(0:(K-1), 1:K);
-plt.yticks(0:(J-1), 1:J);
+plotZ(Matrix(Z'))
 plt.savefig("img/Z.pdf")
 plt.close()
 
 ### Heatmap ###
-x = randn(30000, 32) * 2;
-x[:, 3] .= NaN;
+Y = randn(30000, 32) * 2;
+Y[:, 3] .= NaN;
 
-cm = plt.cm_get_cmap(:bwr)
-cm[:set_under](color=:blue)
-cm[:set_over](color=:red)
-cm[:set_bad](color=:black)
-plt.imshow(x, vmin=-2, vmax=2, aspect="auto", cmap=cm)
-plt.plt[:colorbar]();
-
+plotY(Y)
 plt.savefig("img/heatmap.pdf")
+plt.close()
 
 # yZ plot
-plt.figure()
+
+plt.subplot2grid((1, 10), (0, 0), colspan=3)
+plotZ(Matrix(Z))
+plt.xticks(rotation=:horizontal)
+plt.subplot2grid((1, 10), (0, 3), colspan=7)
+plotY(Matrix(Y'));
+plt.yticks(pyRange(J), 1:J);
+plt.xticks(rotation=:vertical)
+plt.tight_layout()
+plt.savefig("img/yZ.pdf")
