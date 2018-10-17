c8ba3a88a44edaefe97b133904f6cd9a9eec8fde
Arthur Lui <luiarthur@gmail.com>
2018-10-16 21:57:36 -0700

    pyplot tests with with kde (self-made) and multi-axis labelling

--- sandbox/pyplot/plots.jl
+++ sandbox/pyplot/plots.jl
@@ -1,6 +1,7 @@
 import PyPlot
 const plt = PyPlot
 printKeys(x) = [print("$k  ") for k in keys(x)]
+using Distributions
 #=
 plt.ioff() # turn off interactive graphics. turn on with plt.ion()
 =#
@@ -50,7 +51,6 @@ plt.savefig("img/heatmap.pdf")
 plt.close()
 
 # yZ plot
-
 plt.subplot2grid((1, 10), (0, 0), colspan=3)
 plotZ(Matrix(Z))
 plt.xticks(rotation=:horizontal)
@@ -60,3 +60,41 @@ plt.yticks(pyRange(J), 1:J);
 plt.xticks(rotation=:vertical)
 plt.tight_layout()
 plt.savefig("img/yZ.pdf")
+
+# kde
+function kde(x::Vector{T}; from::T=minimum(x), to::T=maximum(x), numPoints::Int=10000,
+             bw::Float64=0.0, kernel::Function=z->pdf(Normal(), z)) where {T <: Number}
+  n = length(x)
+  if bw == 0.0
+    bw = std(x) * n ^ (-0.2) * 1.06
+  end
+  x_grid = collect(range(from, stop=to, length=numPoints))
+  return Dict(:dx => [sum(kernel.((xi .- x) / bw)) / (n * bw) for xi in x_grid], :x => x_grid)
+end
+
+dist = Gamma(3, 200)
+@time x = rand(dist, 10000);
+@time d = kde(x, numPoints=100, bw=100.);
+plt.plot(d[:x], pdf.(dist, d[:x]), label=:truth, color=:blue)
+plt.plot(d[:x], d[:dx], color=:orange, label=:kde)
+x_ci = quantile(x, [.025, .975])
+plt.fill_between(d[:x][x_ci[1] .< d[:x] .< x_ci[2]], d[:dx][x_ci[1] .< d[:x] .< x_ci[2]],
+                color=:orange)
+plt.tight_layout()
+plt.legend()
+plt.savefig("img/den.pdf")
+
+
+# Axis everywhere
+# https://matplotlib.org/examples/api/two_scales.html
+fig, ax1 = plt.subplots()
+ax1[:plot](randn(100))
+ax1[:set_xlabel]("x")
+# Make the y-axis label, ticks and tick labels match the line color.
+ax1[:set_ylabel](:leftLabel, color=:blue)
+ax1[:tick_params]("y", colors=:blue)
+
+ax2 = ax1[:twinx]()
+ax2[:set_ylabel](:blabla)
+ax2[:tick_params](:blable)
+plt.savefig("img/axisGalore.pdf")
