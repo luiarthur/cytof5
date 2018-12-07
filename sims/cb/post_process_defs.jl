d759bfa1f33261ed4fb0dd864f3f9e3fbb8a5271
luiarthur <luiarthur@gmail.com>
2018-12-07 10:12:10 -0800

    bug fixes

--- sims/cb/post_process_defs.jl
+++ sims/cb/post_process_defs.jl
@@ -140,10 +140,10 @@ function post_process(path_to_output)
     for j in 1:J
       println("i: $i, j: $j")
       numMissing = sum(cbData[i][:, j] .=== NaN)
-      h = maximum(util.density(yimp[i][:, j])[:y])
+      h = maximum(util.density(y_imputed[1][i][:, j])[:y])
       util.plot(util.density([cbData[i][:, j]; fill(-10, numMissing)], na=true, bw=.3),
                 col="red", xlim=[-8,8], main="Y sample: $(i), marker: $(j),
-                missing: $numMissing", bty="n", fg="grey", ylim=c(0,h))
+                missing: $numMissing", bty="n", fg="grey", ylim=[0,h])
       for iter in 1:B
         yimp = y_imputed[iter]
         util.lines(util.density(yimp[i][:, j]), col=util.rgba("blue", .5))
