1f1f4ff1f7c884927e2344ca880f1e65bc95be25
Arthur Lui <luiarthur@gmail.com>
2019-01-24 15:42:01 -0800

    made a logistic regression vb example

---
+++ sandbox/pytorch/vb/logistic/logistic.py
@@ -0,0 +1,130 @@
+import torch
+import time
+import math
+import copy
+import datetime
+import matplotlib.pyplot as plt 
+import numpy as np
+
+import sys
+sys.path.append('..')
+
+# Define data type
+dtype = torch.float64
+
+# Define device-type (cpu / gpu)
+device = torch.device("cpu")
+
+def loglike(y, x, b):
+    p = torch.sigmoid(b[0] + b[1] * x)
+    return torch.distributions.Bernoulli(p).log_prob(y).sum()
+
+def log_prior(b):
+    return torch.distributions.Normal(0., 5.).log_prob(b).sum()
+
+def log_q(b, v):
+    """
+    b: parameters in logistic regression
+    v: variational parameters, in real scale
+    """
+    return sum([torch.distributions.Normal(v[j][0], torch.exp(v[j][1])).log_prob(b[j]) for j in range(len(b))])
+
+def fit(y, x, niters=1000, nsamps=10, lr=1e-3, minibatch_size:int=0, seed=1, eps=1e-8, init=None):
+    # Set random seed for reproducibility
+    torch.manual_seed(seed)
+
+    # Number of obs
+    N = len(y)
+    if minibatch_size <= 0 or minibatch_size > N:
+        minibatch_size = N
+
+    # zero all the gradients
+    def zero_grads(params):
+        for param in params:
+            if param.grad is not None:
+                param.grad.zero_()
+
+    def sample_elbo_once(state):
+        if 0 < minibatch_size < N:
+            idx = torch.randperm(N)[:minibatch_size]
+            y_minibatch = y[idx]
+            x_minibatch = x[idx]
+        else:
+            y_minibatch = y
+            x_minibatch = x
+        
+        # log likelihood
+        J = len(state)
+        # b = torch.stack([torch.distributions.Normal(s[0], torch.exp(s[1])).sample() for s in state])
+        eta = [torch.distributions.Normal(0, 1).sample() for j in range(J)]
+        # print([[eta[j] * state[j][1] + state[j][0]] for j in range(J)])
+        b = torch.stack([eta[j] * state[j][1] + state[j][0] for j in range(J)])
+        # b = torch.stack([[e * torch.exp(w) + m for (m, w) in s] for (s, e) in zip(state, eta)])
+        ll = (loglike(y_minibatch, x_minibatch, b) / minibatch_size) * N
+        out_elbo = ll + log_prior(b) + log_q(b, state)
+        zero_grads(state)
+        loss = -out_elbo
+        loss.backward()
+        out_grad = [s.grad for s in state]
+        return (out_elbo, out_grad)
+
+
+    # Initialize parameters.
+    def create_vp():
+        b = torch.randn(2, device=device, dtype=dtype)
+        b.requires_grad = True
+        return b
+
+    if init is not None:
+        state = copy.deepcopy(init)
+    else:
+        state = [create_vp() for xx in range(2)]
+
+    elbo_history = []
+    lr_history = [lr]
+
+    for t in range(niters):
+        elbo_mean = 0.0
+        grad_means = [torch.empty(s.size(), dtype=dtype) for s in state]
+        for i in range(nsamps):
+            e, g = sample_elbo_once(state)
+            elbo_mean += e.item() / nsamps
+            for (gm, g) in zip(grad_means, g):
+                gm[0] += g[0] / nsamps
+                gm[1] += g[1] / nsamps
+
+        elbo_history.append(elbo_mean)
+        now = datetime.datetime.now()
+        print('{} | iteration: {} / {} | elbo: {}'.format(now,
+              t + 1, niters, elbo_history[-1]))
+        print('state: {}'.format(state))
+
+        with torch.no_grad():
+            for (s, gm) in zip(state, grad_means):
+                s[0].data.sub_(gm[0] * lr)
+                s[1].data.sub_(gm[1] * lr)
+                # s[1].data.sub_((gm[1] + 1) * lr)
+
+        if t > 0 and abs(elbo_history[-1] - elbo_history[-2]) / N < eps:
+            break
+
+    return {'state': state, 'lr': lr_history, 'elbo': elbo_history}
+  
+
+if __name__ == '__main__':
+    torch.manual_seed(1)
+
+    N = 1000
+    x = np.random.randn(N)
+    b0 = .5
+    b1 = 2
+    p = 1 / (1 + np.exp(-(b0 + b1 * x)))
+    y = (p > np.random.rand(N)) * 1.0
+
+    x = torch.tensor(x)
+    y = torch.tensor(y)
+
+    out = fit(y, x, lr=1e-3, minibatch_size=0, niters=500, nsamps=10, seed=1, eps=1e-8, init=None)
+    plt.plot(out['elbo']); plt.show()
+
+
