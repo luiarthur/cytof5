cd13065f748045c2f29132e92d0fde79147e2fc9
Arthur Lui <luiarthur@gmail.com>
2019-02-04 15:28:43 -0800

    corrected the plot

--- sims/cb/plot_center.R
+++ sims/cb/plot_center.R
@@ -7,19 +7,24 @@ I = length(y)
 Y = Reduce(rbind, y)
 J = NCOL(Y)
 
+good_markers = c(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, TRUE, FALSE,
+                 TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+                 TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE,
+                 TRUE, FALSE, TRUE)
+
 THRESH = seq(.1, 1, by=.1)
 for (thresh in THRESH) {
-  M = matrix(NA, I, J)
-  for (i in 1:I) for (j in 1:J) {
-    M[i, j] = mean(abs(y[[i]][, j]) < thresh, na.rm=TRUE)
-  }
-
   pdf('results/thresh/thresh_' %+% thresh %+% '_.pdf')
   par(mfrow=c(I, 1))
-  for (i in 1:I) {
-    plot(M[i, ], type='h', main='proportion of cells s.t. |y_{inj}| < ' %+% thresh,
-         xlab='markers in sample ' %+% thresh, ylab='proportion')
-  }
+  M = sapply(as.list(1:I), function(i) {
+    x = apply(y[[i]][, good_markers], 1, function(yin) sum(abs(yin) < thresh, na.rm=TRUE))
+    plot(table(x), ylab='counts',
+         xlab='number of markers (j)',
+         main='counts of near-zero expressions in j markers for sample ' %+% i)
+    x
+  })
   par(mfrow=c(1, 1))
   dev.off()
 }
+
+
