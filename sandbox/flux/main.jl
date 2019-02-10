937956e70134658d72710eb983a3b0f5e4679985
Arthur Lui <luiarthur@gmail.com>
2019-02-09 18:13:25 -0800

    made a linear regression example in flux.jl

--- sandbox/flux/main.jl
+++ sandbox/flux/main.jl
@@ -1,5 +1,8 @@
 include("model.jl")
 using Flux, Flux.Tracker
+import Random
+
+Random.seed!(0);
 
 # Generate Data
 N = 1000
@@ -7,16 +10,26 @@ x = randn(N)
 b0 = 2.0
 b1 = -3.0
 sig = 0.5
-y = b0 .+ b1 .* x .+ sig
+y = b0 .+ b1 .* x .+ randn(N) * sig
 
-vps = Dict(:b0 => VarParam(), :b1 => VarParam(), :sig => VarParam())
-loss(y, x, vps) = -elbo(y, x, vps)
+vp = VP()
+loss(y, x) = -elbo(y, x, vp) / N
 
-grads = Tracker.gradient(vps -> loss(x, y, vps), param(vps))
-opt = ADAM(.1)
+params = Tracker.Params([getfield(vp, fn) for fn in fieldnames(typeof(vp))])
+grads = Tracker.gradient(() -> loss(y, x), params)
 
-Tracker.update!(opt, grads)
+opt = ADAM(1e-1)
+minibatch_size = 100
+niters = 10000
 
-for p in params
-  Tracker.update!(opt, p, grads[p])
+@time for i in 1:niters
+  idx = sample(1:N, minibatch_size)
+  Flux.train!(loss, params, [(y[idx], x[idx])], opt)
+  if i % (niters / 10) == 0
+    println("Progress: $(i)/$(niters) | ELBO: $(-loss(y, x).data)")
+  end
 end
+
+sig_post = exp.([rsample(vp.log_sig) for b in 1:1000])
+println("b0_mean: $(vp.b0[1].data) | b1_mean: $(vp.b1[1].data), | sig_mean: $(mean(sig_post).data)")
+println("Loss: $(loss(y, x).data)")
--- sandbox/flux/model.jl
+++ sandbox/flux/model.jl
@@ -1,55 +1,38 @@
-# https://fluxml.ai/Flux.jl/stable/training/optimisers/
 using Flux, Flux.Tracker
 using Distributions
 
-struct VarParam
-  m::Flux.Tracker.TrackedReal{Float64}
-  log_s::Flux.Tracker.TrackedReal{Float64}
-  VarParam() = new(param(0.0), param(0.0))
+struct VP
+  b0
+  b1
+  log_sig
+
+  VP() = new(param(randn(2)), param(randn(2)), param(randn(2)))
 end
 
-
-get_vd(vp) = Normal(vp.m, exp(vp.log_s))
-
-function logpdf_logx(logx, logpdf)
-  x = exp(logx)
-  return logpdf(x) + logx
+function lpdf_logx(dist, logx)
+  return logpdf(dist, exp(logx)) + logx
 end
 
-function rsample(vp::VarParam)
-  return randn() * exp(vp.log_s) + vp.m
-end
-
-function loglike(y, x, params)
-  b0 = params[:b0]
-  b1 = params[:b1]
-  sig = params[:sig]
-
+function loglike(y::Vector{S}, x::Vector{S}, b0::T, b1::T, sig::T) where {T, S}
   return sum(logpdf.(Normal.(b0 .+ b1 .* x, sig), y))
 end
 
-lpdf_sig(x) = logpdf(Gamma(1, 1), x)
-
-function log_prior(real_params)
-  b0 = real_params[:b0]
-  b1 = real_params[:b1]
-  log_sig = real_params[:sig]
-
-  return logpdf(Normal(0, 1), b0) + logpdf(Normal(0, 1), b1) + logpdf_logx(log_sig, lpdf_sig)
+function log_p(b0::T, b1::T, log_sig::T) where T
+  return logpdf(Normal(0, 1), b0) + logpdf(Normal(0, 1), b1) + lpdf_logx(Gamma(1, 1), log_sig)
 end
 
-function log_q(real_params, vp)
-  return sum([logpdf(get_vd(vp[key]), real_params[key]) for key in keys(vp)])
+lpdf_vp(p, x) = logpdf(Normal(p[1], exp(p[2])), x)
+function log_q(b0::T, b1::T, log_sig::T, vp::VP) where T
+  return lpdf_vp(vp.b0, b0) + lpdf_vp(vp.b1, b1) + lpdf_vp(vp.log_sig, log_sig)
 end
 
-function to_param_space(real_params)
-  params = deepcopy(real_params)
-  params[:sig] = exp(params[:sig])
-  return params
+rsample(p) = randn() * exp(p[2]) + p[1]
+function elbo(y::Vector{T}, x::Vector{T}, vp) where {T}
+  b0 = rsample(vp.b0)
+  b1 = rsample(vp.b1)
+  log_sig = rsample(vp.log_sig)
+  sig = exp(log_sig)
+
+  return loglike(y, x, b0, b1, sig) + log_p(b0, b1, log_sig) - log_q(b0, b1, log_sig, vp)
 end
 
-function elbo(y, x, vp)
-  real_params = Dict(k => rsample(vp[k]) for k in keys(vp))
-  params = to_param_space(real_params)
-  return loglike(y, x, params) + log_prior(real_params) + log_q(real_params, vp)
-end
