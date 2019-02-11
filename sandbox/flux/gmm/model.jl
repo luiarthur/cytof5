26bf8c790a8caf5e3a6b591673c7a86717d70dc3
Arthur Lui <luiarthur@gmail.com>
2019-02-11 14:17:04 -0800

    made a lpdf dirichlet that is full ranked

--- sandbox/flux/gmm/model.jl
+++ sandbox/flux/gmm/model.jl
@@ -1,6 +1,7 @@
 using Flux, Flux.Tracker
 using Distributions
 import LinearAlgebra.logdet
+import SpecialFunctions.lgamma
 
 struct VP
   m
@@ -15,22 +16,40 @@ function logsumexp(logx::T) where T
   return mx + log(sum(exp.(logx .- mx)))
 end
 
+function logsumexp0p(logx::T) where T
+  mx = max(maximum(logx), 0.0)
+  return mx + log(exp(-mx) + sum(exp.(logx .- mx)))
+end
+
+# Test
+# for i in 1:1000
+#   x = randn(100)
+#   @assert all(abs(logsumexp([0.0; x]) .- logsumexp0p(x)) < 1e-8)
+# end
+
 function lpdf_logx(dist::D, logx::T) where {D, T}
   return logpdf(dist, exp(logx)) + logx
 end
 
 function softmax_safe(x::T) where T
-  return exp.(x .- logsumexp(x))
+  logsumexp_x = logsumexp(x)
+  return exp.(x .- logsumexp_x)
 end
 
 """
-Transform a real vector of size K to a simplex of size K + 1.  This is for
-identifiability. The first dimension of the simplex is 0 in the real vector
-(hence it is one dimension less).
+Transform a real vector to a simplex. The sum of the resulting simplex
+is less than 1, and one minus that sum is the probability of the first 
+element in the K+1 dimensional simplex.
 """
 function softmax_fullrank(x::T) where T
-  x_aug = [0.0; x]
-  return softmax_safe(x_aug)
+  logsumexp0p_x = logsumexp0p(x)
+  return exp.(x .- logsumexp0p_x)
+end
+
+function lpdf_Dirichlet_fullrank(a::A, p::T) where {A, T}
+  out = lgamma(sum(a)) - sum(lgamma.(a)) + sum((a[2:end] .- 1.0) .* log.(p))
+  out += (a[1] - 1.0) * log1p(-sum(p))
+  return out
 end
 
 # TODO: Test
@@ -42,11 +61,30 @@ function lpdf_real_simplex(dist::D, real_simplex::T) where {D, T}
   for i in 1:dim
     for j in 1:dim
       if i == j
-        jacobian[i, j] = p[i+1] * (1 - p[i+1]) # TODO
+        jacobian[i, j] = p[i] * (1 - p[i]) # TODO
       else
-        jacobian[i, j] = jacobian[j, i] = -p[i+1] * p[j+1] # TODO
+        jacobian[i, j] = jacobian[j, i] = -p[i] * p[j] # TODO
       end
     end
   end
-  return logpdf(dist, p) + logdet(jacobian)
+  return lpdf_Dirichlet_fullrank(dist.alpha, p) + logdet(jacobian)
 end
+
+
+# Test
+K = 30
+a = collect(1:K)
+
+x = randn(K - 1)
+y = param(x)
+
+@time for i in 1:100
+  A = lpdf_real_simplex(Dirichlet(a), x)
+  B = lpdf_real_simplex(Dirichlet(a), y)
+  @assert abs(A - B) < 1e-6
+
+  C = lpdf_Dirichlet_fullrank(a, softmax_fullrank(x))
+  D = logpdf(Dirichlet(a), softmax([0.; x]))
+  @assert abs(C - D) < 1e-6
+end
+
