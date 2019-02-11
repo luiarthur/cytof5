a8ade46999b85bec2cd7e82f29256b728d80e710
Arthur Lui <luiarthur@gmail.com>
2019-02-11 12:54:29 -0800

    added lpdf for real simplex's / dirichlet

--- sandbox/flux/gmm/model.jl
+++ sandbox/flux/gmm/model.jl
@@ -1,5 +1,6 @@
 using Flux, Flux.Tracker
 using Distributions
+import LinearAlgebra.logdet
 
 struct VP
   m
@@ -22,6 +23,30 @@ function softmax_safe(x::T) where T
   return exp.(x .- logsumexp(x))
 end
 
+"""
+Transform a real vector of size K to a simplex of size K + 1.  This is for
+identifiability. The first dimension of the simplex is 0 in the real vector
+(hence it is one dimension less).
+"""
+function softmax_fullrank(x::T) where T
+  x_aug = [0.0; x]
+  return softmax_safe(x_aug)
+end
+
+# TODO: Test
 function lpdf_real_simplex(dist::D, real_simplex::T) where {D, T}
-  return # TODO
+  dim = length(real_simplex)
+  p = softmax_fullrank(real_simplex) # dimensions: dim + 1
+  xtype = typeof(real_simplex[1])
+  jacobian = zeros(xtype, dim, dim)
+  for i in 1:dim
+    for j in 1:dim
+      if i == j
+        jacobian[i, j] = p[i+1] * (1 - p[i+1]) # TODO
+      else
+        jacobian[i, j] = jacobian[j, i] = -p[i+1] * p[j+1] # TODO
+      end
+    end
+  end
+  return logpdf(dist, p) + logdet(jacobian)
 end
