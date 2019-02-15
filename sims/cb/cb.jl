378f1eb07ebc23b158ec842c048f6b2bbcfa3d02
Arthur Lui <luiarthur@gmail.com>
2019-02-15 10:15:39 -0800

    impute closer

--- sims/cb/cb.jl
+++ sims/cb/cb.jl
@@ -136,7 +136,8 @@ Cytof5.Model.logger("\nGenerating priors ...");
                                         sig2_prior=InverseGamma(3.0, 2.0),
                                         alpha_prior=Gamma(0.1, 10.0),
                                         # yQuantiles=[.1, .25, .4], pBounds=[.05, .8, .05],
-                                        yQuantiles=[0.0, .01, .05], pBounds=[.05, .8, .05],
+                                        # yQuantiles=[0.0, .01, .05], pBounds=[.05, .8, .05], # far
+                                        yQuantiles=[0.0, .35, .7], pBounds=[.05, .8, .05], # near
                                         # TODO: CHECK
                                         similarity_Z=Cytof5.Model.sim_fn_abs(10000),
                                         probFlip_Z=2.0 / (dat.J * K_MCMC),
