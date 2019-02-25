d5849e4d7290842bc1b8f8f2bcf85fb82aaf9ed4
Arthur Lui <luiarthur@gmail.com>
2019-02-25 14:09:01 -0800

    much faster compared to pytorch

--- sandbox/flux/logisticreg/main.jl
+++ sandbox/flux/logisticreg/main.jl
@@ -5,7 +5,7 @@ import Random
 Random.seed!(0);
 
 # Generate Data
-N = 5000
+N = 1000
 x = randn(N)
 b0 = .5
 b1 = 2.0
@@ -20,7 +20,7 @@ params = Tracker.Params([getfield(vp, fn) for fn in fieldnames(typeof(vp))])
 # grads = Tracker.gradient(() -> loss(y, x), params)
 
 opt = ADAM(1e-1)
-niters = 10000
+niters = 1000
 
 @time for i in 1:niters
   idx = sample(1:N, minibatch_size)
--- sandbox/flux/logisticreg/model.jl
+++ sandbox/flux/logisticreg/model.jl
@@ -10,27 +10,28 @@ end
 
 sigmoid(x) = 1.0 / (1.0 + exp(-x))
 
-function loglike(y, x, b0, b1, N)
+function loglike(y, x, params, N)
+  b0 = params[:b0]
+  b1 = params[:b1]
   prob = clamp.(sigmoid.(b0 .+ b1 .* x), .001, .999)
   return sum(y .* log.(prob) .+ (1 .- y) .* log1p.(-prob)) * (N / length(y))
   # return sum(logpdf.(Bernoulli.(prob), y)) * (N / length(y))
 end
 
-function log_p(b0, b1)
-  # return logpdf(Normal(0, 1), b0) + logpdf(Normal(0, 1), b1)
+function log_p(params)
   return 0.0
 end
 
 lpdf_vp(p, x) = logpdf(Normal(p[1], exp(p[2])), x)
-function log_q(b0, b1, vp)
-  return lpdf_vp(vp.b0, b0) + lpdf_vp(vp.b1, b1)
+function log_q(params, vp)
+  return lpdf_vp(vp.b0, params[:b0]) + lpdf_vp(vp.b1, params[:b1])
 end
 
 rsample(p) = randn() * exp(p[2]) + p[1]
-function elbo(y, x, vp, N)
-  b0 = rsample(vp.b0)
-  b1 = rsample(vp.b1)
+rsample(vp::VP) = Dict(:b0 => rsample(vp.b0), :b1 => rsample(vp.b1))
 
-  return loglike(y, x, b0, b1, N) + log_p(b0, b1) - log_q(b0, b1, vp)
+function elbo(y, x, vp, N)
+  params = rsample(vp)
+  return loglike(y, x, params, N) + log_p(params) - log_q(params, vp)
 end
 
