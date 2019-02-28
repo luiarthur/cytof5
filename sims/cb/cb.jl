71d3738f42a727172680e2d11ce2ba55e8c1bc35
Arthur Lui <luiarthur@gmail.com>
2019-02-28 11:03:04 -0800

    turn on the dic computation

--- sims/cb/cb.jl
+++ sims/cb/cb.jl
@@ -190,8 +190,8 @@ nsamps_to_thin(nsamps::Int, nmcmc::Int) = max(1, div(nmcmc, nsamps))
                           thins=[2, nsamps_to_thin(10, MCMC_ITER)],
                           # fix=[:sig2], # TODO: remove this?
                           nmcmc=MCMC_ITER, nburn=BURN,
-                          # computeLPML=true, computeDIC=true,
-                          computeLPML=true, computeDIC=false,
+                          # computeLPML=true, computeDIC=false,
+                          computeLPML=true, computeDIC=true,
                           computedden=true, thin_dden=nsamps_to_thin(200, MCMC_ITER),
                           #computedden=true, thin_dden=1,
                           use_repulsive=USE_REPULSIVE,
