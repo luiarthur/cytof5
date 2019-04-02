5cfe1510dea619543dd18e0acfc8bdf22391a9af
Arthur Lui <luiarthur@gmail.com>
2019-04-02 10:41:05 -0700

    added a test

--- sandbox/flux/gmm-sbt/test.jl
+++ sandbox/flux/gmm-sbt/test.jl
@@ -8,3 +8,10 @@ x = param([.1, .2, .3])
 p = SB.transform(x)
 @assert abs(SB.logabsdetJ(x, p) - (-5.5850)) < 1e-4
 @assert abs(logpdf(Dirichlet(ones(4)), p) + SB.logabsdetJ(x, p) - (-3.7933)) < 1e-4
+
+# TEST
+y = param(ones(3))
+p = SB.transform(y)
+z = sum(p)
+back!(z)
+y.tracker.grad
