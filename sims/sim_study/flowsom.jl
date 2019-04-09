b1007afc4a4e75d74d93cfb02eabdc06ec40e60d
Arthur Lui <luiarthur@gmail.com>
2019-04-09 16:55:02 -0700

    made some progress with flowsom.jl

--- sims/sim_study/flowsom.jl
+++ sims/sim_study/flowsom.jl
@@ -8,17 +8,41 @@ using BSON
 @rimport rcommon
 @rimport cytof3
 
-SIMDAT_PATH = ARGS[1]
-RESULTS_DIR = ARGS[2]
 if length(ARGS) == 0
   # Where to get data
-  SIMDAT_PATH = ""
+  SIMDAT_PATH = "results/sim-paper/sim_Nfac500_K5_KMCMC02/output.bson"
   # Where to put results from FlowSOM analysis
   RESULTS_DIR = "results/flowsom/"
 else
-  SIMDIR = ARGS[1]
+  SIMDAT_PATH = ARGS[1]
   RESULTS_DIR = ARGS[2]
 end
 
+# TODO:
 # mkpath(OUTPUT_DIR)
-replaceMissing(yi, x) = (yi[isnan.(out)] .= x)
+
+# Load simulated data
+@time y = BSON.load(SIMDAT_PATH)[:simdat][:y]
+
+# replace missing values with -20
+replaceMissing(yi, x) = (yi[isnan.(yi)] .= x)
+for yi in y; replaceMissing(yi, -20); end
+
+# Combine into one matrix Y
+Y = vcat(y...)
+J = size(Y, 2)
+@rput Y
+
+# Create flowframe
+ff_Y = R"colnames(Y) <- 1:NCOL(Y); flowCore::flowFrame(Y)"
+@time fSOM = FlowSOM.FlowSOM(ff_Y,
+                             # Input options:
+                             colsToUse = 1:J,
+                             # Metaclustering options:
+                             #nClus = 20,
+                             maxMeta=20,
+                             # Seed for reproducible results:
+                             seed = 42)
+
+
+
