1a328c9c90627aada71852c54d91191710203524
Arthur Lui <luiarthur@gmail.com>
2019-04-11 08:39:18 -0700

    created metrics script

---
+++ sims/sim_study/metrics.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+import sys
+import os
+import subprocess
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+
+
+def readFile(fname):
+    with open(fname, 'r') as f:
+        contents = f.readlines()
+    return contents
+
+
+def writeFile(x, fname):
+    with open(fname, 'w') as f:
+        f.write(x)
+
+
+if __name__ == '__main__':
+    def parse(lines, header):
+        y = list(filter(lambda x: "{} => ".format(header) in x, lines))[0]
+        y = y.split("=>")[1].strip()
+        return float(y)
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 metrics <path-to-results>")
+        sys.exit(1)
+    else:
+        results_dir = sys.argv[1]
+        if len(sys.argv) > 2:
+            excludes = sys.argv[2]
+        else:
+            excludes = "NOTHING"
+        print("excludes: {}".format(excludes))
+
+        sim_dirs = os.listdir(results_dir)
+        sim_dirs = list(filter(lambda d: 'KMCMC' in d, sim_dirs))
+        sim_dirs = list(filter(lambda d: not re.search(excludes, d), sim_dirs))
+
+        Nfac = np.array([int(re.findall(r'(?<=Nfac)\d+', d)[0])
+                      for d in sim_dirs])
+        Nfac = set(Nfac)
+        print('Nfac: {}'.format(Nfac))
+
+        for nfac in Nfac:
+            DIC = []
+            pD = []
+            Dmean = []
+            LPML = []
+            
+            sim_dirs_nfac = filter(lambda d: 'Nfac{}'.format(nfac) in d, sim_dirs)
+            sim_dirs_nfac = sorted(sim_dirs_nfac)
+        
+            K = np.array([int(re.findall(r'(?<=KMCMC)\d+', d)[0])
+                          for d in sim_dirs_nfac])
+            order = np.argsort(K)
+
+            # DIC
+            for d in sim_dirs_nfac:
+                c = readFile('{}/{}/log.txt'.format(results_dir, d))
+                DIC.append(parse(c, 'DIC'))
+                pD.append(parse(c, 'pD'))
+                Dmean.append(parse(c, 'Dmean'))
+                LPML.append(parse(c, 'LPML'))
+
+            # Plot
+            METRICS_DIR = '{}/metrics/Nfac{}/'.format(results_dir, nfac)
+            os.makedirs(METRICS_DIR, exist_ok=True)
+
+            LPML = np.array(LPML)
+            plt.figure()
+            plt.plot(K[order], LPML[order], linestyle='--', marker='o')
+            plt.ylabel('LPML')
+            plt.xlabel('K')
+            plt.xticks(K, rotation=90)
+            plt.savefig('{}/lpml.pdf'.format(METRICS_DIR), bbox_inches='tight')
+            plt.close()
+
+            DIC = np.array(DIC)
+            plt.figure()
+            plt.plot(K[order], DIC[order], linestyle='--', marker='o')
+            plt.ylabel('DIC')
+            plt.xlabel('K')
+            plt.xticks(K, rotation=90)
+            plt.savefig('{}/dic.pdf'.format(METRICS_DIR), bbox_inches='tight')
+            plt.close()
+
+            pD = np.array(pD)
+            plt.figure()
+            plt.plot(K[order], pD[order], linestyle='--', marker='o')
+            plt.ylabel('pD')
+            plt.xlabel('K')
+            plt.xticks(K, rotation=90)
+            plt.savefig('{}/pD.pdf'.format(METRICS_DIR), bbox_inches='tight')
+            plt.close()
+
+            Dmean = np.array(Dmean)
+            plt.figure()
+            plt.plot(K[order], Dmean[order], linestyle='--', marker='o')
+            plt.ylabel('Dmean')
+            plt.xlabel('K')
+            plt.xticks(K, rotation=90)
+            plt.savefig('{}/Dmean.pdf'.format(METRICS_DIR), bbox_inches='tight')
+            plt.close()
+
