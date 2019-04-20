e7850fb4cbc530b9edf6fab4f4623675962d9461
Arthur Lui <luiarthur@gmail.com>
2019-04-20 12:30:15 -0700

    made correction to sig2 update

--- src/Model/update_sig2.jl
+++ src/Model/update_sig2.jl
@@ -1,6 +1,8 @@
 function update_sig2(i::Int, s::State, c::Constants, d::Data)
   ss = 0.0
 
+  cardinality = 0
+
   for j in 1:d.J
     for n in 1:d.N[i]
       k = s.lam[i][n]
@@ -8,11 +10,13 @@ function update_sig2(i::Int, s::State, c::Constants, d::Data)
         z = s.Z[j, k]
         l = s.gam[i][n, j]
         ss += (s.y_imputed[i][n, j] - mus(z, l, s, c, d)) ^ 2
+        cardinality += 1
       end
     end
   end
 
-  newShape = shape(c.sig2_prior) + d.J * d.N[i] / 2
+  # newShape = shape(c.sig2_prior) + d.J * d.N[i] / 2
+  newShape = shape(c.sig2_prior) + cardinality / 2
   newScale = scale(c.sig2_prior) + ss / 2
 
   if c.sig2_range == [0, Inf]
