17db9f775efef5ff88b13175310558772ae62e00
luiarthur <luiarthur@gmail.com>
2019-04-22 16:30:04 -0700

    1: updated deps. 2: added function to loadCB data from text file.

--- src/Model/util.jl
+++ src/Model/util.jl
@@ -73,3 +73,49 @@ ig = InverseGamma(1002, 1001)
 mean(ig), std(ig)
 @time x = [rand_uptrunc_ig(ig, .3) for i in 1:10000];
 =#
+
+
+"""
+Read CB data from a text file.
+Text file must have following properties:
+- first line is: "N: N1 N2 N3"
+- second line is the name of the markers separated by a single space
+- the remainder of the file is the marker expression levels for each cell 
+  by sample. No line breaks are necessary between samples. Each line should
+  contain expression levels for each cell equal to the number of markers.
+
+
+Usage
+=====
+
+y  = loadCB(path_to_data)
+"""
+function loadCB(path_to_data; ElType::Type=Float64)
+  return open(path_to_data) do f
+    # read number of cells in first line
+    N = readline(f)
+    N = split(N)[2:end]
+    N = parse.(Int, N)
+
+    # Number of samples
+    I = length(N)
+
+    # Read marker names
+    markers = split(readline(f))
+
+    # Read data into separate samples
+    y = [begin
+           yi = Vector{ElType}[]
+           for _ in 1:N[i]
+             line = split(readline(f))
+             line = [replace(obs, "NA" => "NaN") for obs in line]
+             line = parse.(ElType, line)
+             append!(yi, [line])
+           end
+           Matrix(hcat(yi...)')
+         end for i in 1:I]
+
+    return y
+  end
+end
+
