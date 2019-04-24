5d2e3eb017dea5da6797044358b5cc220d5f4ad5
Arthur Lui <luiarthur@gmail.com>
2019-04-23 19:12:06 -0700

    added some example stuff in test

--- src/VB/test.jl
+++ src/VB/test.jl
@@ -12,5 +12,22 @@ struct Theta
   alpha
 end
 
-
 (d::Dict)(x) = println(x)
+
+##########################
+using Flux, Flux.Tracker
+module Test
+
+mutable struct Bob{T <: AbstractFloat, A <: AbstractArray{T}}
+  a::A
+  Bob(TT::Type, AA::Type) = new{TT, AA{TT}}()
+end
+end
+
+b1 = Test.Bob(Float16, Array)
+b1.a = randn(2)
+println(typeof(b1))
+
+b2 = Test.Bob(Float32, TrackedArray)
+b2.a = param(randn(Float32, 3))
+println(typeof(b2))
