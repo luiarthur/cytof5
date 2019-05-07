049287a1ae31c077ffd3de3d4096b9d290e37350
Arthur Lui <luiarthur@gmail.com>
2019-05-06 18:23:18 -0700

    this version works.

--- src/VB/test/gmm.jl
+++ src/VB/test/gmm.jl
@@ -12,9 +12,15 @@ end
 
 function State(K::Integer)
   state = State()
-  state.w = ADVI.ModelParam(K - 1, "simplex")
-  state.m = ADVI.ModelParam(K, "real")
-  state.s = ADVI.ModelParam(K, "positive")
+
+  # NOTE: this does not work!
+  # state.w = ADVI.ModelParam(K - 1, "simplex")
+  # state.m = ADVI.ModelParam(K, "real")
+  # state.s = ADVI.ModelParam(K, "positive")
+
+  state.w = ADVI.ModelParam((1, K - 1), "simplex")
+  state.m = ADVI.ModelParam((1, K), "real")
+  state.s = ADVI.ModelParam((1, K), "positive")
 
   return state
 end
@@ -40,26 +46,28 @@ function loglike(tran::State, y::Vector{Float64})
   K = length(tran.m)
   N = length(y)
 
-  m = reshape(tran.m, 1, K)
-  s = reshape(tran.s, 1, K)
-  w = reshape(tran.w, 1, K)
+  # NOTE: This does not work!
+  # m = reshape(tran.m, 1, K)
+  # s = reshape(tran.s, 1, K)
+  # w = reshape(tran.w, 1, K)
+  # return sum(ADVI.lpdf_gmm(reshape(y, N, 1), m, s, w, dims=2, dropdim=true))
 
-  return sum(ADVI.lpdf_gmm(reshape(y, N, 1), m, s, w, dims=2, dropdim=true))
+  return sum(ADVI.lpdf_gmm(reshape(y, N, 1), tran.m, tran.s, tran.w, dims=2, dropdim=true))
 end
 
 function logprior(real::State, tran::State, mp::State)
   lp = 0.0
 
-  # lp += sum(ADVI.compute_lpdf(Normal(0, 10), tran.m))
-  lp += sum(ADVI.lpdf_normal.(tran.m, 0.0, 10.0))
+  lp += sum(ADVI.compute_lpdf(Normal(0, 10), tran.m))
+  # lp += sum(ADVI.lpdf_normal.(tran.m, 0.0, 10.0))
 
-  # lp += sum(ADVI.compute_lpdf(Gamma(1, 1), tran.s))
-  lp += sum(ADVI.lpdf_gamma.(tran.s, 1.0, 1.0))
+  lp += sum(ADVI.compute_lpdf(Gamma(1, 1), tran.s))
+  # lp += sum(ADVI.lpdf_gamma.(tran.s, 1.0, 1.0))
   lp += sum(ADVI.logabsdetJ(mp.s, real.s, tran.s))
 
   K = length(tran.w)
-  # lp += sum(ADVI.compute_lpdf(Dirichlet(ones(K)), tran.w))
-  lp += sum(ADVI.lpdf_dirichlet(tran.w, ones(K)))
+  lp += sum(ADVI.compute_lpdf(Dirichlet(ones(K)), tran.w))
+  # lp += sum(ADVI.lpdf_dirichlet(tran.w, one.(tran.w)))
   lp += sum(ADVI.logabsdetJ(mp.w, real.w, tran.w))
 
   return lp
--- src/VB/test/main.jl
+++ src/VB/test/main.jl
@@ -1,7 +1,6 @@
 include("gmm.jl")
 import Random, Dates
 
-Random.seed!(0);
 ShowTime() = Dates.format(Dates.now(), "yyyy-mm-dd HH:MM:SS")
 
 # Generate Data
@@ -15,19 +14,27 @@ y = [begin
        rand(Normal(m[k], s[k]))
      end for i in 1:N]
 
+Random.seed!(1);
 mp = State(K)
 loss(y) = -compute_elbo(mp, y, N) / N
 ps = ADVI.vparams(mp)
 
-opt = ADAM(1e-2)
+opt = ADAM(1e-1)
 minibatch_size = 500
 niters = 10000
-for t in 1:niters
+@time for t in 1:niters
   idx = Distributions.sample(1:N, minibatch_size, replace=false)
   Flux.train!(loss, ps, [(y[idx], )], opt)
-  # if t % 10 == 0
+  if t % 100 == 0
     println("$(ShowTime()) | $(t)/$(niters) ")
-  # end
+  end
 end
 
+samps = [rsample(mp)[2] for i in 1:100]
+m_post = vcat([s.m for s in samps]...)
+s_post = vcat([s.s for s in samps]...)
+w_post = vcat([s.w for s in samps]...)
 
+println("m mean: $(mean(m_post, dims=1)) | true: $m")
+println("s mean: $(mean(s_post, dims=1)) | true: $s")
+println("w mean: $(mean(w_post, dims=1)) | true: $w")
