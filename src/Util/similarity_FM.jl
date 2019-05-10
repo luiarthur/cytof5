0c6ce51b084f88507e1cf6b22aa287dbe59518de
Arthur Lui <luiarthur@gmail.com>
2019-05-09 22:52:24 -0700

    made second version of similarity_FM

--- src/Util/similarity_FM.jl
+++ src/Util/similarity_FM.jl
@@ -20,39 +20,38 @@ compute similarity of two feature matrices
 Arguments
 =========
 - Z1: point estimate of Z from FAM (with K1 columns)
-- Z2: point estimate of Z from FAM (with K2 columns)
 - W1: point estimate of W from FAM (with K1 columns)
-- W2: point estimate of Z from FAM (with K2 columns)
+- Z2: point estimate of Z from FAM (with K2 columns)
 """
-function similarity_FM(Z1::Matrix{T}, Z2::Matrix{T},
-                       W1::Matrix{F}, W2::Matrix{F};
+function similarity_FM(Z1::Matrix{T}, W1::Matrix{F}, Z2::Matrix{T};
                        z_diff=sumabsdiff) where {T <: Integer, F <: AbstractFloat}
   # cache dimensions
   I, K1 = size(W1)
   J = size(Z1, 1)
-  K2 = size(W2, 2)
+  K2 = size(Z2, 2)
 
   # Make sure dimensions match
   @assert size(Z1, 1) == size(Z2, 1) == J
   @assert size(Z1, 2) == size(W1, 2) == K1
-  @assert size(Z2, 2) == size(W2, 2) == K2
-  @assert size(W1, 1) == size(W2, 1) == I
 
-  # Iterate over columns of Z1 -- the smaller matrix
-  @assert K1 <= K2
+  # Iterate over columns of Z1 -- the larger matrix
+  @assert K1 >= K2
    
-  function engine(Z1::Matrix{T}, Z2::Matrix{T},
-                  W1::Vector{F}, W2::Vector{F}; s::Float64=0.0)
-    if length(Z1) == 0
+  function engine(z1::Matrix{T}, w1::Vector{F}, z2::Matrix{T}; s::Float64=0.0)
+    if length(z1) == 0
       return s
+    elseif length(z2) == 0
+      return engine(z1[:, 2:end], w1[2:end], Z2, s=s)
     else
-      k2, d = closest_column(Z1[:, 1], Z2, z_diff=z_diff)
-      engine(Z1[:, 2:end], Z2[:, 1:end .!= k2],
-             W1[2:end], W2[1:end .!= k2], s=s+d*W1[1])
+      k2, d = closest_column(z1[:, 1], z2, z_diff=z_diff)
+      engine(z1[:, 2:end], w1[2:end], z2[:, 1:end .!= k2], s=s+d*w1[1])
     end
   end
 
-  s = sum(engine(Z1, Z2, W1[i, :], W2[i, :]) for i in 1:I)
+  s = sum(begin 
+            ord_1 = sortperm(W1[i, :], rev=true)
+            engine(Z1[:, ord_1], W1[i, ord_1], Z2)
+          end for i in 1:I)
   
   return s
 end
--- test/Util_tests.jl
+++ test/Util_tests.jl
@@ -1,18 +1,25 @@
+#=
+using Test
+include("../src/Util/Util.jl")
+import .Util: similarity_FM, meanabsdiff, sumabsdiff
+=#
+
 import Cytof5.Util: similarity_FM, meanabsdiff, sumabsdiff
 
 @testset "Util similarity_FM" begin
   I = 3
   J = 20
-  K1 = 40
-  K2 = 50
+  K1 = 5
+  K2 = 4
 
   W1 = rand(I, K1)
-  W2 = rand(I, K2)
+  W1 ./= sum(W1, dims=2)
 
   Z1 = Int.(rand(J, K1) .> .5)
   Z2 = Int.(rand(J, K2) .> .5)
 
-  @time similarity_FM(Z1, Z2, W1, W2, z_diff=meanabsdiff)
+  @time s = similarity_FM(Z1, W1, Z2)
+  @assert s <= J * I
 
   @test true
 end
