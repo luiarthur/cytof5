f2c69e5441365ecccb3141b44f0a2b63270d6fda
Arthur Lui <luiarthur@gmail.com>
2019-05-21 15:23:57 -0700

    flowsom for CB

--- sims/cb/flowsom.jl
+++ sims/cb/flowsom.jl
@@ -1,32 +1,44 @@
 using Cytof5
 using RCall, Distributions, Random
 using JLD2, FileIO
+include("PreProcess.jl")
+
+function loadSingleObj(objPath)
+  data = load(objPath)
+  return data[collect(keys(data))[1]]
+end
+
+function replaceMissing(yi, x)
+  out = deepcopy(yi)
+  out[isnan.(out)] .= x
+  return out
+end
+
 
 @rimport FlowSOM
 @rimport flowCore
-R"source('../FlowSOM/est_Z_from_clusters.R')"
 
 if length(ARGS) == 0
-  # Where to get data
-  DAT_PATH = ""
-  # Where to put results from FlowSOM analysis
-  RESULTS_DIR = "results/sim-paper/flowsom/"
+  DATA_PATH = "data/cytof_cb_with_nan.jld2"
+  RESULTS_DIR = "results/cb-paper/flowsom/"
 else
-  SIMDAT_PATH = ARGS[1]
+  DATA_PATH = ARGS[1]
   RESULTS_DIR = ARGS[2]
 end
 
 # TODO:
 mkpath(RESULTS_DIR)
 
-# Load simulated data
-@time simdat = BSON.load(SIMDAT_PATH)[:simdat]
-@time y = simdat[:y]
+# Load data
+@time y = loadSingleObj(DATA_PATH)
+goodColumns, J = PreProcess.preprocess!(y, maxNanOrNegProp=.9, maxPosProp=.9,
+                                        subsample=0.0, rowThresh=-6.0)
+Cytof5.Model.logger("good columns: $goodColumns")
+y_orig = deepcopy(y)
 
 # replace missing values with `REPLACEMENT`
 REPLACEMENT = -6
-replaceMissing(yi, x) = (yi[isnan.(yi)] .= x)
-for yi in y; replaceMissing(yi, REPLACEMENT); end
+y = [replaceMissing(yi, REPLACEMENT) for yi in y_orig]
 
 # Combine into one matrix Y
 Y = vcat(y...)
@@ -57,6 +69,7 @@ addGridLines = function(Z, s=1) abline(v=1:NCOL(Z) + .5, h=1:NROW(Z) + .5, col='
 
 library(rcommon)
 library(cytof3)
+source('../FlowSOM/est_Z_from_clusters.R')
 
 idx_upper = cumsum(N)
 idx_lower = c(1,idx_upper[-I]+1)
@@ -67,12 +80,12 @@ fsEst = as.list(1:$I)
 fsClus = as.numeric(fSOMClus)
 
 mult=1
-plotPng($RESULTS_DIR %+% 'YZ%03d_FlowSOM.png', s=10)
+plotPng($RESULTS_DIR %+% 'Y%03d_FlowSOM.png', s=10)
 for (i in 1:$I) {
   clus = fsClus[idx[i,1]:idx[i,2]]
   print(length(unique(clus))) # Number of clusters learned
   clus = relabel_clusters(clus)
-  my.image($(y)[[i]][order(clus),], col=blueToRed(9), zlim=zlim, addL=TRUE,
+  my.image($(y_orig)[[i]][order(clus),], col=blueToRed(9), zlim=zlim, addL=TRUE,
            na.color='black', cex.y.leg=1, xlab='cell types',  ylab='cells',
            cex.lab=1.5, cex.axis=1.5, xaxt='n',
            f=function(z) {
@@ -81,25 +94,4 @@ for (i in 1:$I) {
            })
 }
 dev.off()
-
-println("Computing ARI ...")
-true.clus.ls <- $(simdat[:lam])
-fs.clus.ls = lapply(1:I, function(i) fsClus[idx_lower[i]:idx_upper[i]])
-
-ARI = sapply(1:I, function(i) {
-  ari(true.clus.ls[[i]], fs.clus.ls[[i]])
-})
-
-ARI_all = 'FlowSOM'=ari(unlist(true.clus.ls), fsClus)
-print(ARI)
-print(ARI_all)
-
-sink($RESULTS_DIR %+% "ari.txt")
-  println("ARI:")
-  print(ARI)
-  println("ARI all:")
-  print(ARI_all)
-sink()
 """
-
-
