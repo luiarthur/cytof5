69888374539c33a1b649e95344f9bbd59d28e05b
Arthur Lui <luiarthur@gmail.com>
2019-10-02 20:50:16 -0700

    starting to dev

--- sandbox/dev/dev.jl
+++ sandbox/dev/dev.jl
@@ -1,2 +1,12 @@
 using Revise
 using Cytof5
+
+J = 32
+N = [300, 100, 200]
+K = 8
+L = Dict(0 => 5, 1 => 3)
+
+simdat = Cytof5.Model.genData(J, N, K, L)
+d = Cytof5.Model.Data(simdat[:y])
+c = Cytof5.Model.defaultConstants(d, K, L)
+s = Cytof5.Model.genInitialState(c, d)
