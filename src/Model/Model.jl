af80595980f90c377d1b8bc844dd4afde071a900
Arthur Lui <luiarthur@gmail.com>
2019-10-10 14:50:37 -0700

    cleaning

--- src/Model/Model.jl
+++ src/Model/Model.jl
@@ -193,7 +193,13 @@ function cytof5_fit(init::State, c::Constants, d::Data;
 
   if isinf(compute_loglike(init, c, d, normalize=true))
     println("Warning: Initial state yields likelihood of zero.")
-    println("It is likely the case that the initialization of missing values is not consistent with the provided missing mechanism. The MCMC will almost certainly reject the initial values and sample new ones in its place.")
+    msg = """
+    It is likely the case that the initialization of missing values
+    is not consistent with the provided missing mechanism. The MCMC
+    will almost certainly reject the initial values and sample new
+    ones in its place.
+    """
+    println(join(split(msg), " "))
   else
     println("")
   end
