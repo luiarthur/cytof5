7b0a3f8f2af24815c44de59a423dd17466a79c95
Arthur Lui <luiarthur@gmail.com>
2019-10-19 10:55:37 -0700

    try to do this in stan

---
+++ sims/cb/stan/cytof.R
@@ -0,0 +1,57 @@
+library(rstan)
+library(rcommon)
+
+compiled_stan_model = "compiled_stan_model.rds"
+stan_model = "model.stan"
+
+if (file.exists(compiled_stan_model)) {
+  cat("Reading previously-compiled STAN model...", "\n")
+  cmodel <- readRDS(compiled_stan_model)
+} else {
+  cat("Compiling STAN model...", "\n")
+  system.time(cmodel <- stan_model(stan_model))
+  saveRDS(cmodel, compiled_stan_model)
+}
+
+use_vb = TRUE
+
+y = readRDS('../data/cytof_cb.rds')
+subsamp_prop = .01
+y = lapply(y, function(yi) {
+  Ni = NROW(yi)
+  yi[sample(1:Ni, as.integer(Ni * subsamp_prop)), ]
+})
+
+good_markers = c(T, F, T, F, T, F, T, T, F, T,
+                 F, T, T, T, F, T, T, F, F, T,
+                 F, T, T, T, T, F, T, F, T, T,
+                 F, T)
+
+Y = Reduce(rbind, y)
+Y = Y[, which(good_markers)]
+Nsum = NROW(Y)
+J = NCOL(Y)
+N = sapply(y, NROW)
+I = length(y)
+
+m = matrix(0, Nsum, J)
+m[is.na(Y)] <- 1
+Y[is.na(Y)] <- 0
+group = unlist(sapply(1:I, function(i) rep(i, N[i])))
+data = list(J=J, I=I, K=10, N=Nsum, group=group, L0=5, L1=3,
+            m=m, y=Y)
+data$a_W = rep(1/data$K, data$K)
+data$a_eta0 = rep(1/data$L0, data$L0)
+data$a_eta1 = rep(1/data$L1, data$L1)
+
+init = list(mu0=matrix(0, data$J, data$K),
+            mu1=matrix(0, data$J, data$K),
+            sigma=rep(.1, data$I),
+            alpha=1,
+            v=rep(1/data$K, data$K))
+
+out <- stan(file=stan_model, data=data, init=list(init),
+            iter=2000, warmup=1000, chains=1)
+
+samps = extract(out)
+plot(samps$lp__, type='l')
