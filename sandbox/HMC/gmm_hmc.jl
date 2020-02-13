5302d4f2f69bf1c1090857815dced1a0f046b70b
Arthur Lui <luiarthur@gmail.com>
2020-02-12 22:45:06 -0800

    gmm hmc can compile and run. but not getting correct solution.

--- sandbox/HMC/gmm_hmc.jl
+++ sandbox/HMC/gmm_hmc.jl
@@ -30,9 +30,9 @@ State(K::Int) = State(param(randn(K)),  # mu
                       param(randn(K)),  # sig
                       param(randn(K - 1)))  # stickbreak_w
 
-function sim_data(; N=100, mu=[-1, 1], sig=[1, .5], w=[.3, .7], seed=missing)
+function sim_data(; N=100, mu=[-1, 1], sig=[.1, .3], w=[.3, .7], seed=missing)
   if !ismissing(seed)
-    Random.seed(seed)
+    Random.seed!(seed)
   end
 
   K = length(mu)
@@ -40,9 +40,7 @@ function sim_data(; N=100, mu=[-1, 1], sig=[1, .5], w=[.3, .7], seed=missing)
   @assert sum(w) == 1
 
   cluster_labels = wsample(1:K, w, N)
-  mu = mu[cluster_labels]
-  sig = sig[cluster_labels]
-  y = randn(N) .* sig + mu
+  y = randn(N) .* sig[cluster_labels] + mu[cluster_labels]
  
   return Dict(:cluster_labels => cluster_labels,
               :mu => mu, :sig => sig, :w => w, :y => y)
@@ -51,44 +49,74 @@ end
 ### MAIN ###
 
 # Simulate data
-simdat = sim_data()
+simdat = sim_data(N=50)
 N = length(simdat[:y])
-K = 3
+K = 2
 
 # Log posterior
 function logpost(s::State)
   K = length(s.mu)
   mu = reshape(s.mu, 1, K)
   sig = reshape(exp.(s.log_sig), 1, K)
-  w = SB_transform(s.stickbreak_w)
+  # w = SB_transform(s.stickbreak_w)  # FIXME: I expected this would work.
+  real_w = reshape(s.stickbreak_w, 1, K - 1)
+  w = SB_transform(real_w)
 
   ll = (ADVI.lpdf_normal.(reshape(simdat[:y], N, 1), mu, sig) .+
-        log.(reshape(w, 1, K)))
+        log.(w))
   ll = sum(ADVI.logsumexp(ll, dims=2))
 
   lp = (sum(ADVI.compute_lpdf(Normal(0, 1), mu)) +
-        sum(ADVI.compute_lpdf(LogNormal(0, 1), sig)) +
-        sum(ADVI.compute_lpdf(Dirichlet(K, 1 / K), w)))
-
-  a = sum(s.log_sig)
-  b = sum(SB_logabsdetJ(s.stickbreak_w, w))
+        sum(ADVI.compute_lpdf(LogNormal(0, .1), sig)) +
+        sum(ADVI.compute_lpdf(Dirichlet(K, K), w)))
 
   lpabsjacobian = (sum(s.log_sig) +
-                   sum(SB_logabsdetJ(s.stickbreak_w, w)))
+                   sum(SB_logabsdetJ(real_w, w)))
 
   return ll + lp + lpabsjacobian
 end
 
 state = State(K)
 _ = logpost(state)
-num_leapfrog_steps = 10
-eps = .01
 
-# FIXME?
-_ = HMC.hmc_update(state, logpost, num_leapfrog_steps, eps)
+function simulate(init; nburn, nsamps, eps, num_leapfrog_steps, 
+                  kappa=nothing, momentum_sd=1.0)
+  state = deepcopy(init)
+
+  samps = [state for i in 1:nsamps]
+  log_prob_hist = zeros(nsamps)
+
+  for i in 1:(nburn + nsamps)
+    print("\rProgress: $i / $(nburn + nsamps)")
+    eta = (kappa == nothing ? eps : eps * i ^ -kappa)
+    state, curr_log_prob = HMC.hmc_update(state, logpost,
+                                          num_leapfrog_steps, eta,
+                                          momentum_sd=momentum_sd)
+    if i > nburn
+      samps[i - nburn] = state
+      log_prob_hist[i - nburn] = curr_log_prob
+    end
+  end
+  println()
+
+  return Dict(:samps => samps, :log_prob => log_prob_hist)
+end
 
 
+# Compile
+_ = simulate(state, nburn=1, nsamps=1, num_leapfrog_steps=1, eps=.1, kappa=.7)
 
+# Simulate
+@time out = simulate(state, nburn=500, nsamps=100,
+                     num_leapfrog_steps=2^1, eps=.1)
+
+# FIXME: runs. but can't get correct answer...
+println(simdat[:mu], simdat[:sig], simdat[:w])
+println()
+
+println("mu post mean: $(mean([o.mu for o in out[:samps]]))")
+println("sig post mean: $(mean([exp.(o.log_sig) for o in out[:samps]]))")
+println("w post mean: $(mean([SB_transform(o.stickbreak_w) for o in out[:samps]]))")
+
+R"plot($(out[:log_prob]), type='l')"
 end # module GmmHmcTest
-
-# Maybe try this in python???
