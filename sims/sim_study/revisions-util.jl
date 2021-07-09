b3a6161999906d4734afce09a4a2e6f56dcb2dc7
Arthur Lui <luiarthur@gmail.com>
2021-07-09 14:42:00 -0600

    added sim data

--- sims/sim_study/revisions-util.jl
+++ sims/sim_study/revisions-util.jl
@@ -3,19 +3,23 @@ using BSON
 
 include("salso/salso.jl")
 
-function bytes2float64(byte::Vector{UInt8})
+function frombytes(byte::Vector{UInt8}, T)
     n = length(byte)
     @assert mod(n, 8) == 0
     counter = 1
-    out = Float64[]
+    out = T[]
     while counter < n
         idx = counter:counter+7
-        x = reinterpret(Float64, byte[idx])
+        x = reinterpret(T, byte[idx])
         append!(out, x)
         counter += 8
     end
     return out
 end
+
+bytes2int8(byte::Vector{UInt8}) = frombytes(byte, Int8)
+bytes2int64(byte::Vector{UInt8}) = frombytes(byte, Int64)
+bytes2float64(byte::Vector{UInt8}) = frombytes(byte, Float64)
 
 function get_Z_lam(path)
     results = BSON.parse(path)
@@ -63,4 +67,24 @@ function write_summary(outdir, summary)
             writedlm(io, summary.W[i])
         end
     end
+end
+
+function get_data(path; I=3)
+    data = BSON.parse(path)
+    y = [let
+             yvec = bytes2float64(data[:simdat][:y][i][:data])
+             size = data[:simdat][:y][i][:size]
+             reshape(yvec, size...)
+         end for i in 1:I]
+
+    y_complete = [let
+             yvec = bytes2float64(data[:simdat][:y_complete][i][:data])
+             size = data[:simdat][:y_complete][i][:size]
+             reshape(yvec, size...)
+         end for i in 1:I]
+
+    lam = [bytes2int64(data[:simdat][:lam][i][:data])
+           for i in 1:I]
+
+    return (y=y, y_complete=y_complete, lam=lam)
 end
--- sims/sim_study/revisions.jl
+++ sims/sim_study/revisions.jl
@@ -19,3 +19,18 @@ write_summary("$(outdir)/sim-small-fam-mcmc", small_sim_summary)
 write_summary("$(outdir)/sim-large-fam-mcmc", large_sim_summary)
 # write_summary("$(outdir)/cb-fam-mcmc", cb_summary)
 
+# Write simulation truths.
+small_data = get_data(path_to_small_sim_results)
+large_data = get_data(path_to_large_sim_results)
+
+for sym in (:lam, :y, :y_complete)
+    I = 3
+    for i in 1:I
+        open("$(outdir)/sim-small-fam-mcmc/$(sym)-$(i)-data.txt", "w") do io
+            writedlm(io, small_data[sym][i])
+        end
+        open("$(outdir)/sim-large-fam-mcmc/$(sym)-$(i)-data.txt", "w") do io
+            writedlm(io, large_data[sym][i])
+        end
+    end
+end
